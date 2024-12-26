802460c3654a194bfb43e0a0555953779b611329
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2024-12-26 02:51:24 +0000

    ui fixes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import { menuData } from '../constant/menuData';
 import { Link } from 'react-router-dom';
 
 
 
 const Sidebar = () => {
+
+  const [active, setActive ] = useState('Dashboard');
 
   const handleLogout = async () => {
     localStorage.removeItem('loginData'); 
@@ -15,26 +18,28 @@ const Sidebar = () => {
   return (
   
     <div className='w-[250px] min-h-full border-r-[1px] flex flex-col mb-4'>
-      <div className="px-1 py-6">
+      <div className="px-1 py-6 h-full">
         <div className="flex gap-4 items-center ml-[20px]">
           <img src="/logo1.svg" alt="" className="" />
           <img src="/banicoop.svg" alt="" className="" />
         </div>
 
          { menuData.map((item) => (
-          <Link to={item.url} key={item.name} className="flex items-center gap-2 p-3 hover:bg-[#fafafa] hover:text-[#6922D1] rounded-full mt-2">
-            <img src={item.icon} alt="" className="md:ml-[10px] text-bgPurple" />
-            <span className='hidden md:block text-sm'>{item.name}</span>
+          <Link 
+            to={item.url} key={item.name} 
+            onClick={() => setActive(item.name)}
+            className={`flex items-center text-[#000] font-[500] gap-2 p-2 lg:p-3 rounded-full mt-2 ${active === item.name ? 'text-[#fff] bg-[#6922D1]': ''}`}>
+            <img src={item.icon} alt="" className="md:ml-[10px]" style={{ color: '#6922D1' }} />
+            <span className='hidden md:block md:text-xs lg:text-sm '>{item.name}</span>
           </Link>
          ))}
-
+    </div>
 
     {/* BOTTOM */}
       <div className="mt-auto border-t-[1px] flex items-center px-3 py-6 gap-3 cursor-pointer" onClick={handleLogout}>
         <img src='/logout.svg' alt="" className="md:ml-[10px] text-bgPurple" />
         <span className='hidden md:block text-sm'>Log Out</span>
       </div>
-    </div>
     </div>
   )
 }
