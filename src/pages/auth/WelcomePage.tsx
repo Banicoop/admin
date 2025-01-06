24ef771e334f31fe052cbc05c9bb0efe835e7369
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2025-01-06 19:30:07 +0000

    admin status update

--- src/pages/auth/WelcomePage.tsx
+++ src/pages/auth/WelcomePage.tsx
@@ -7,14 +7,14 @@ const WelcomePage = () => {
 
 
   return (
-    <div className='flex w-full flex-col items-center justify-center p-3 md:p-[4rem] gap-[2rem]'>
-        <h1 className='text-2xl md:text-3xl font-semibold flex self-start justify-start flex-col'>Empowering <br/>Communities through<br/><span className="text-bgPurple">Financial Freedom</span></h1>
+    <div className='flex w-full flex-col items-center justify-center h-full p-4 lg:p-[4rem] gap-2 lg:gap-[2rem]'>
+        <h1 className='text-sm lg:text-3xl font-semibold flex self-start justify-start flex-col'>Empowering <br/>Communities through<br/><span className="text-bgPurple">Financial Freedom</span></h1>
 
-        <h4 className="text-sm">Welcome to Banicoop’s Admin Panel, where you play a key role in helping individuals and communities manage their finances, save collaboratively, and achieve financial stability.</h4>
+        <h4 className="text-xs lg:text-sm">Welcome to Banicoop’s Admin Panel, where you play a key role in helping individuals and communities manage their finances, save collaboratively, and achieve financial stability.</h4>
 
         <div className="bg-[#F0E9F9] rounded-lg p-2 md:p-6 flex flex-col gap-3">
             <span className="text-bgPurple font-semibold">Get Started</span>
-            <p className="text-sm">This admin panel provides powerful tools and insights, giving you full control to oversee and manage your organization’s financial activities, monitor transactions, and support our user community.</p>
+            <p className="text-xs lg:text-sm">This admin panel provides powerful tools and insights, giving you full control to oversee and manage your organization’s financial activities, monitor transactions, and support our user community.</p>
         </div>
 
         <div className="flex justify-end self-end">
--- src/sections/admins/AdminTable.tsx
+++ src/sections/admins/AdminTable.tsx
@@ -1,10 +1,12 @@
-import React, { useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 // import { adminData } from '../../constant/menuData';
 import  ActionBtn  from '../../components/buttons/ActionBtn';
 import Table from '../../components/tables/Table';
 import { useDispatch, useSelector } from 'react-redux';
 import { Dispatch } from '../../redux/store';
-import { getAllAdmin } from '../../redux/slice/adminSlice';
+import { changeStatus, getAllAdmin } from '../../redux/slice/adminSlice';
+import BasicModal from '../../components/modals/DeleteModal';
+import { motion } from 'framer-motion';
 
 
 const columns = [
@@ -36,28 +38,34 @@ const columns = [
 
 const AdminTable = () => {
 
+  const [ open, setOpen ] = useState(false);
+  const [selectedAdminId, setSelectedAdminId] = useState<string | null>(null);
+
   const dispatch = useDispatch<Dispatch>();
   const { allAdmin, status } = useSelector((state: any) => state.admin)
 
-  useMemo(() => {
+  useEffect(() => {
     dispatch(getAllAdmin())
   }, [dispatch])
 
-  console.log((allAdmin.admins));
+
 
   let adminData = allAdmin?.admins;
 
   const changeAdminStatus = () => {
-    
+    if (selectedAdminId) {
+      dispatch(changeStatus(selectedAdminId)); 
+      setOpen(false); 
+    }
   }
 
   const renderRow = (item: any) => (
     <tr className='border-b border-gray-100 even:bg-slate-50 text-sm py-4'>
       <td className="flex items-center gap-4 py-4 px-2">
-        <div className="flex items-center gap-4">
+        <span className="flex items-center gap-4">
           <img src={item?.profileImage || '/admin/profile.png'} alt="" className="h-5 w-5 rounded-full" />
           <span className="">{`${item.firstName} ${item.lastName}`}</span>
-        </div>
+        </span>
       </td>
       <td className='py-4'>{item?.role === 'super_admin' ? 'Super Admin': 'Admin'}</td>
       <td className='py-4'>
@@ -65,10 +73,14 @@ const AdminTable = () => {
       </td>
       {/* <td className='py-4'>{item?.last_login}</td> */}
       <td className='py-4'>
-        <div className="flex items-center gap-4">
+        <span className="flex items-center gap-4">
           <ActionBtn text='View' onClick={() => {}} className='px-4 py-2 text-sm rounded-3xl bg-[#E6E6E680] text-[#6922D1] border-[1px] border-[#6922D1] cursor-pointer'/>
-          <ActionBtn text='Deactivate' onClick={changeAdminStatus} className='px-4  py-2 rounded-3xl bg-[#fff] text-[#6922D1] border-[1px] border-[#6922D1] cursor-pointer w-[95px]'/>
-        </div>
+
+          <ActionBtn 
+            text={item?.disabled === false ? 'Deactivate': 'Activate'} 
+            onClick={() => setSelectedAdminId(item._id)} 
+            className='px-4  py-2 rounded-3xl bg-[#fff] text-[#6922D1] border-[1px] border-[#6922D1] cursor-pointer w-[95px]'/>
+        </span>
       </td>
 
     </tr>
@@ -76,8 +88,29 @@ const AdminTable = () => {
 
 
   return (
-    <div className=''>
+    <div className='relative'>
       <Table columns={columns} data={adminData} renderRow={renderRow} status={status}/>
+
+      { open &&
+           <BasicModal onClose={() => setOpen(false)} open={open}>
+              <motion.div 
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                  duration: 0.4,
+                  scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+              }}
+              className="flex flex-col p-3">
+                <h2 className=''>Are you sure want to change admin status?</h2>
+
+                <div className="flex p-4 justify-between items-center">
+                  <ActionBtn text='Yes, Proceed' onClick={changeAdminStatus} className='px-4 py-2 text-sm rounded-3xl bg-[#E6E6E680] text-[#6922D1] border-[1px] border-[#6922D1] cursor-pointer'/>
+
+                  <ActionBtn text='Cancel' onClick={() => setOpen(false)} className='px-4  py-2 rounded-3xl bg-[#fff] text-[#6922D1] border-[1px] border-[#6922D1] cursor-pointer w-[95px]'/>
+                </div>
+              </motion.div>
+           </BasicModal>
+      }
     </div>
   )
 }
